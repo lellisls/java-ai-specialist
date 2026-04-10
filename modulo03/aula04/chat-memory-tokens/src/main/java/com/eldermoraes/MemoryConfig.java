7d528a7fb249373ee0e246abce4918067d5cbb68
Elder Moraes <elder@eldermoraes.com>
2026-04-10 10:56:51 -0300

    Fixes

--- modulo03/aula04/chat-memory-tokens/src/main/java/com/eldermoraes/MemoryConfig.java
+++ modulo03/aula04/chat-memory-tokens/src/main/java/com/eldermoraes/MemoryConfig.java
@@ -3,18 +3,21 @@ package com.eldermoraes;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.TokenWindowChatMemory;
 import dev.langchain4j.model.TokenCountEstimator;
+import dev.langchain4j.model.openai.OpenAiTokenCountEstimator;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.Produces;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class MemoryConfig {
 
-    @Inject
-    TokenCountEstimator  tokenCountEstimator;
+    @ConfigProperty(name ="quarkus.langchain4j.openai.chat-model.model-name")
+    String modelName;
 
     @Produces
     public ChatMemoryProvider chatMemoryProvider() {
+        TokenCountEstimator tokenCountEstimator = new OpenAiTokenCountEstimator(modelName);
+
         return memoryId -> TokenWindowChatMemory.builder()
                 .id(memoryId)
                 .maxTokens(1000, tokenCountEstimator)
